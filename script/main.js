da68e1fa803d0f1953af835c224b28d4087094ba
ssthil <ssthil@gmail.com>
2015-06-30 18:37:00 +0800

    initial push

---
+++ script/main.js
@@ -0,0 +1,66 @@
+/**
+* @author Senthil Sivaramakrishnan
+* @desc reports layout
+* @date 27-05-2015
+*/
+$(function(){
+	//accordion for the left navigation
+	$('.category-header').click(function(){
+		$(this).next('.reports-list').slideToggle(300);
+		$(this).find('span').toggleClass('accordion-icon-active');
+	});
+	//create span element
+	$('.category-header').append("<span class='accordion-icon accordion-icon-normal'></span>");
+
+	//date picker
+    $('input[name="daterange"]').daterangepicker();
+    
+    //report title
+    $('.reports-list span').on('click', function(){
+    	$('.report-title').text($(this).text());
+    })
+    
+
+    /*$('#reportrange span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+ 
+    $('#reportrange').daterangepicker({
+        format: 'MM/DD/YYYY',
+        startDate: moment().subtract(29, 'days'),
+        endDate: moment(),
+        minDate: '01/01/2012',
+        maxDate: '12/31/2015',
+        dateLimit: { days: 60 },
+        showDropdowns: true,
+        showWeekNumbers: true,
+        timePicker: false,
+        timePickerIncrement: 1,
+        timePicker12Hour: true,
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        opens: 'left',
+        drops: 'down',
+        buttonClasses: ['btn', 'btn-sm'],
+        applyClass: 'btn-primary',
+        cancelClass: 'btn-default',
+        separator: ' to ',
+        locale: {
+            applyLabel: 'Submit',
+            cancelLabel: 'Cancel',
+            fromLabel: 'From',
+            toLabel: 'To',
+            customRangeLabel: 'Custom',
+            daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
+            monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            firstDay: 1
+        }
+    }, function(start, end, label) {
+        console.log(start.toISOString(), end.toISOString(), label);
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    });*/
+});
